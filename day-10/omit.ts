92fb72cec0cc5411b68d27dc5da4867ca7b22fbd
lakshmimanaswi2003 <lakshmimanaswithanv@gmail.com>
2026-05-12 11:25:58 +0530

    omit

---
+++ day-10/omit.ts
@@ -0,0 +1,16 @@
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+}
+
+type UserPreview = Omit<User, "email" | "password">
+
+const preview: UserPreview = {
+    name: "Aryan",
+    id: "xyz"
+}
+
+preview.password;
+preview.name;
