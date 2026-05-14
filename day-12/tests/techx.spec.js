bcb911648718345bf3b9c550b61b23712daaefca
lakshmimanaswi2003 <lakshmimanaswithanv@gmail.com>
2026-05-14 18:40:35 +0530

    basics of playwright syntax

--- day-12/tests/techx.spec.js
+++ day-12/tests/techx.spec.js
@@ -1,3 +1,4 @@
+/*
 import { test, expect } from "@playwright/test";
 
 test('check title', async ({ page }) => {
@@ -14,3 +15,28 @@ test('check title', async ({ page }) => {
     await consultBtn.click();
     expect(page.url()).toBe("https://www.techxincorporation.com/contact");
 });
+*/
+
+
+import {test,expect} from "@playwright/test";
+
+test("check",async({page})=>{
+    await page.goto("https://www.techxincorporation.com/");
+
+    await page.locator('//*[@id="root"]/header/div/a[2]').click();
+
+    expect(page.url()).toBe('https://www.techxincorporation.com/contact');
+
+    await page.locator('//*[@id="root"]/div/section[2]/div/div[2]/form/div[1]/input').type("lakshmi",{delay:200});
+
+    await page.locator('//*[@id="root"]/div/section[2]/div/div[2]/form/div[2]/input').type("123@gmail.com",{delay:200});
+
+    await page.locator('//*[@id="root"]/div/section[2]/div/div[2]/form/div[3]/input').type("wipro",{delay:200});
+
+    await page.locator('//*[@id="root"]/div/section[2]/div/div[2]/form/div[4]/select').click("Corporate Training");
+
+    await page.locator('//*[@id="root"]/div/section[2]/div/div[2]/form/div[5]/textarea').type("---");
+
+    await page.getByText("Submit Request").click();
+
+})
